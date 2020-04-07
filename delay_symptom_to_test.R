b5b2d642bc1602cf7e586ba7a1c82743dca80c9f
Jérémie Scire <jeremie.scire@bsse.ethz.ch>
2020-04-07 21:23:36 +0200

    Added data symptoms to hospitalizations

---
+++ delay_symptom_to_test.R
@@ -0,0 +1,39 @@
+rm(lis=ls())
+
+library("ggplot2")
+library("lubridate")
+library("readr")
+library("EpiEstim")
+library("gridExtra")
+library("reshape2")
+
+dataSymptoms <- read.csv(sep=";", stringsAsFactors = F,na.strings = c("","NA"),
+                         "./data/Delay_test_symptoms.csv")
+
+rawDataSymptoms <- dataSymptoms[,-c(1,3)]
+
+rawDataSymptoms <- rawDataSymptoms[complete.cases(rawDataSymptoms),]
+
+startSymptoms <- dmy(rawDataSymptoms$Manifestationsbeginn)
+testDate<- dmy(rawDataSymptoms$Eingangsdatum_LM)
+
+datesSymptoms <- data.frame(startSymptoms=startSymptoms, testDate=testDate )
+datesSymptoms$timeFromSymptomsToTest <- datesSymptoms$testDate - datesSymptoms$startSymptoms
+
+ggplot(datesSymptoms, aes(x=timeFromSymptomsToTest)) +
+  geom_histogram(color="black", fill="grey", binwidth = 1) +
+  theme_bw() +
+  ylab("Count") +
+  xlab("Days") +
+  theme(
+    axis.text.y= element_text(size=17),
+    axis.text.x= element_text(size=17),
+    axis.title.x =  element_text(size=19),
+    axis.title.y =  element_text(size=19)
+  )
+
+plotPath <- "./Fig1_Histogram_delay_symptoms_to_test_report.png"
+ggsave(plotPath, width = 15, height = 15, units = "cm")
+
+mean(as.numeric(datesSymptoms$timeFromSymptomsToTest)) #5.3 on 91 datapoints
+sd(as.numeric(datesSymptoms$timeFromSymptomsToTest)) # 3.8
